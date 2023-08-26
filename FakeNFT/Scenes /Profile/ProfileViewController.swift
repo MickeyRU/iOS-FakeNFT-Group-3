0dc7c70dafcc16971497604cc320c7b243bec949
Pavel Afanasev <afanasyevpv9@gmail.com>
2023-08-26 15:03:09 +0300

    Добавлена кнопка перехода на экран редактирования

--- FakeNFT/Scenes /Profile/ProfileViewController.swift
+++ FakeNFT/Scenes /Profile/ProfileViewController.swift
@@ -1,9 +1,35 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+    
+    private lazy var imageButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "editButton"), for: .normal)
+        button.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+        return button
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        setupViews()
+    }
+    
+    @objc
+    private func editButtonTapped() {
+        print("editButtonTapped")
+        // ToDo: действия по нажатию на кнопку редактирование
+    }
+    
+    private func setupViews() {
+        [imageButton].forEach { view.addViewWithNoTAMIC($0) }
+        
+        NSLayoutConstraint.activate([
+            imageButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 2),
+            imageButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -9),
+            imageButton.heightAnchor.constraint(equalToConstant: 42),
+            imageButton.widthAnchor.constraint(equalTo: imageButton.heightAnchor, multiplier: 1.0)
+        ])
     }
 }

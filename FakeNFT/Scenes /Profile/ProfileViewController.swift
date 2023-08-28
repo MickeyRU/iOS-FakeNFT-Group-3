a164f492fb80660e456678bab0c139e224bee18d
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-08-28 12:49:41 +0300

    Добавлено имя пользователя, описание, ссылка на страничку.

--- FakeNFT/Scenes /Profile/ProfileViewController.swift
+++ FakeNFT/Scenes /Profile/ProfileViewController.swift
@@ -10,14 +10,47 @@ final class ProfileViewController: UIViewController {
     
     private lazy var profileImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .gray
+        imageView.image = UIImage(named: "mockAvatar")
         imageView.layer.cornerRadius = 35
         return imageView
     }()
-        
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Joaquin Phoenix"
+        label.font = UIFont.sfBold22
+        return label
+    }()
+    
+    private let userDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.sfRegular13
+        label.text = "Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT, и еще больше — на моём сайте. Открыт к коллаборациям."
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let urlTextView: UITextView = {
+        let textView = UITextView()
+        let text = "Joaquin Phoenix.com"
+        let attributedString = NSMutableAttributedString(string: text)
+        let linkRange = NSRange(location: 0, length: text.count)
+        attributedString.addAttribute(.link, value: text, range: linkRange)
+
+        textView.attributedText = attributedString
+        textView.isEditable = false
+        textView.isSelectable = true
+        textView.isScrollEnabled = false
+        textView.dataDetectorTypes = .link
+        textView.font = UIFont.sfRegular15
+        textView.textContainer.lineFragmentPadding = 16
+        return textView
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .white
         setupViews()
     }
@@ -28,8 +61,14 @@ final class ProfileViewController: UIViewController {
         // ToDo: действия по нажатию на кнопку редактирование
     }
     
+    @objc
+    private func linkTapped() {
+        print("linkTapped")
+        // ToDo: действия по нажатию на ссылку
+    }
+    
     private func setupViews() {
-        [imageButton, profileImageView].forEach { view.addViewWithNoTAMIC($0) }
+        [imageButton, profileImageView, userNameLabel, userDescriptionLabel, urlTextView].forEach { view.addViewWithNoTAMIC($0) }
         
         NSLayoutConstraint.activate([
             imageButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 2),
@@ -40,7 +79,21 @@ final class ProfileViewController: UIViewController {
             profileImageView.widthAnchor.constraint(equalToConstant: 70),
             profileImageView.heightAnchor.constraint(equalToConstant: 70),
             profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            profileImageView.topAnchor.constraint(equalTo: imageButton.bottomAnchor, constant: 20)
+            profileImageView.topAnchor.constraint(equalTo: imageButton.bottomAnchor, constant: 20),
+            
+            userNameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            userNameLabel.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            
+            userDescriptionLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 20),
+            userDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userDescriptionLabel.trailingAnchor.constraint(equalTo: imageButton.leadingAnchor),
+            userDescriptionLabel.heightAnchor.constraint(equalToConstant: 72),
+            
+            urlTextView.topAnchor.constraint(equalTo: userDescriptionLabel.bottomAnchor, constant: 8),
+            urlTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            urlTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            urlTextView.heightAnchor.constraint(lessThanOrEqualToConstant: 72)
         ])
     }
 }

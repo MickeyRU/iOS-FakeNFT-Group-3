6eed9f024257e1e9d8013e09a51865075abd44cf
Nikita Chagochkin <nick.chagochkin@yandex.ru>
2023-08-31 19:19:57 +0300

    https://trello.com/c/i7LswHlE - Вью каталога поместил в navigationviewcontroller - Добавил таб бар - Сделал простую реализацию алерта

--- FakeNFT/Scenes /Catalog/CatalogViewController.swift
+++ FakeNFT/Scenes /Catalog/CatalogViewController.swift
@@ -3,7 +3,51 @@ import UIKit
 final class CatalogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
+
+        setupNavigationItem()
+    }
+
+    @objc
+    private func sortButtonTapped() {
+        let alert = UIAlertController(
+            title: NSLocalizedString("sort", comment: "Sorting alert title"),
+            message: nil,
+            preferredStyle: .actionSheet)
+
+        let sortByName = UIAlertAction(
+            title: NSLocalizedString("sort.byName", comment: "Sorting alert by name button"),
+            style: .default) { _ in
+            print("Sorted by name")
+
+        }
+        alert.addAction(sortByName)
+
+        let sortByCount = UIAlertAction(
+            title: NSLocalizedString("sort.byAmount", comment: "Sorting alert by amount button"),
+            style: .default) { _ in
+            print("Sorted by count")
+        }
+        alert.addAction(sortByCount)
+
+        let cancel = UIAlertAction(
+            title: NSLocalizedString("close", comment: "Sorting alert close button"),
+            style: .cancel)
+        alert.addAction(cancel)
+
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UI
+extension CatalogViewController {
+    func setupNavigationItem() {
+        let barItem = UIBarButtonItem(
+            image: UIImage(named: "sort"),
+            style: .plain,
+            target: self,
+            action: #selector(sortButtonTapped))
+        barItem.tintColor = .unBlack
+        navigationItem.rightBarButtonItem = barItem
     }
 }

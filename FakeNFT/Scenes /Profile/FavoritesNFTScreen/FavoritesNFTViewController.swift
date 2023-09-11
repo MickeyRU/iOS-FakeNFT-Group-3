1f9c047330b499d148e16b2e2a9994af8948124e
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-09-11 13:20:44 +0300

    Экран "Избранные NFT" - Переход на экран через навигейшен контроллер

--- FakeNFT/Scenes /Profile/FavoritesNFTScreen/FavoritesNFTViewController.swift
+++ FakeNFT/Scenes /Profile/FavoritesNFTScreen/FavoritesNFTViewController.swift
@@ -5,6 +5,12 @@ final class FavoritesNFTViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configNavigationBar()
+    }
+    
+    private func configNavigationBar() {
+        setupCustomBackButton()
+        navigationItem.title = NSLocalizedString("FavoritesNFT", comment: "")
     }
 }
 
